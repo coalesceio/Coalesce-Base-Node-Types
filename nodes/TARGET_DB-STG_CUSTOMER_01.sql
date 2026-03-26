9e03e0d2546065ee3131f940d856ec5025163045
Pranjal Mane <pranjal.mane@dataentrega.com>
2026-03-26 18:50:22 +0530

    Workspace cleanup

--- nodes/TARGET_DB-STG_CUSTOMER_01.sql
+++
@@ -1,8 +0,0 @@
-@id("616b5ff2-7fe8-4255-8476-0382fd4cbbef")
-@nodeType("eb41edb0-7fd7-477a-be93-ba669aa6193d")
-SELECT 
-    CASE 
-        WHEN C_ACCTBAL > 1000 THEN 1 
-        ELSE 0 
-    END AS NUMBER_LITERAL_TEST
-FROM {{ ref('SRC', 'CUSTOMER') }}

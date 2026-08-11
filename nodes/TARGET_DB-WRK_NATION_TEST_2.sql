3eef97085963cfaf8b150c2a69e57d45ec0d21f3
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-11 14:40:09 +0530

    deployenabled false

--- nodes/TARGET_DB-WRK_NATION_TEST_2.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST_2.sql
@@ -5,7 +5,6 @@ SELECT
      "N_NATIONKEY" AS "N_NATIONKEY" @defaultValue("00"),
      "N_NAME" AS "N_NAME" @defaultValue("0''adkfhljkdahjwhk\\`krwk0"),
      "N_REGIONKEY" AS "N_REGIONKEY",
-     "N_COMMENT" AS "N_COMMENT",
      "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
 FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"
 where n_nationkey = {{ parameters.nationkey }}

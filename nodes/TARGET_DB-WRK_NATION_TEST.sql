dc327feb9cb380fc03964ead34114be00bf68620
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-12 14:05:01 +0530

    level 1: Create works..

---
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -0,0 +1,16 @@
+@id("bfd38227-681f-4dca-b148-bd7b6fc79a67")
+@nodeType("62f0cbc1-957e-4ad5-8ca2-5a2d8d6eef62")
+@truncateBefore
+@selectDistinct
+@testsEnabled
+@tests("SELECT 1 FROM {{ this }}", "Before", true)
+@tests("SELECT 1 FROM {{ this }}", "Before", true)
+@tests("SELECT 1 FROM {{ this }}", "After", true)
+@tests("SELECT 1 FROM {{ this }}", "After", true)
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY",
+     "N_NAME" AS "N_NAME" @notNull,
+     "N_REGIONKEY" AS "N_REGIONKEY" @defaultValue("0"),
+     "N_COMMENT" AS "N_COMMENT" @description("Nation comment"),
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"

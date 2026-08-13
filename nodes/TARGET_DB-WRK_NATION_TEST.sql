8d07ca2b38242dcafeb8c212c3c6c5eb6b61fe50
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-13 21:43:58 +0530

    Removed

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -1,6 +1,5 @@
 @id("9b783013-744c-42f2-a215-60c86b27f961")
 @nodeType("62f0cbc1-957e-4ad5-8ca2-5a2d8d6eef62")
-@materializationType("<table | view>")
 SELECT
      "N_NATIONKEY" AS "N_NATIONKEY",
      "N_NAME" AS "N_NAME",

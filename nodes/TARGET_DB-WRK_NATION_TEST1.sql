d629c130c9b7c7390fa355453b7fae83e26535f1
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-13 21:38:24 +0530

    New node - deploy

---
+++ nodes/TARGET_DB-WRK_NATION_TEST1.sql
@@ -0,0 +1,10 @@
+@id("3a5c22ce-f2f7-48ab-8cff-c5a9272dc06a")
+@nodeType("62f0cbc1-957e-4ad5-8ca2-5a2d8d6eef62")
+@selectDistinct
+SELECT
+     "N_NATIONKEY" AS "N_NATIONKEY",
+     "N_NAME" AS "N_NAME",
+     "N_REGIONKEY" AS "N_REGIONKEY",
+     "N_COMMENT" AS "N_COMMENT",
+     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"

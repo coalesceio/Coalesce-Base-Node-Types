bab2177083789f9068b10eef9e2d2e64ec08f292
Tanvi Kamat <tanvi.kamat.contractor@coalesce.io>
2026-08-13 12:04:55 +0530

    Redeployment - Add col, drop, rename, drop not null, desc change, default chnage

--- nodes/TARGET_DB-WRK_NATION_TEST.sql
+++ nodes/TARGET_DB-WRK_NATION_TEST.sql
@@ -2,9 +2,9 @@
 @nodeType("62f0cbc1-957e-4ad5-8ca2-5a2d8d6eef62")
 @description("This node creates the WRK_NATION_TEST table and populates it with distinct records extracted from the NATION_TEST source table.")
 SELECT
-     NATION_TEST."N_NATIONKEY" AS "N_NATIONKEY",
-     "NATION_TEST"."N_NAME" AS "N_NAME" @notNull,
-     "N_REGIONKEY" AS "N_REGIONKEY" @defaultValue("10"),
-     "N_COMMENT" AS "N_COMMENT"@description("Comment"),
-     "N_LOAD_TIMESTAMP" AS "N_LOAD_TIMESTAMP"
+     NATION_TEST."N_NATIONKEY" AS "N_NATIONKEY_RENAMED",
+     "NATION_TEST"."N_NAME" AS "N_NAME",
+     "N_REGIONKEY" AS "N_REGIONKEY" @defaultValue("100"),
+     "N_COMMENT" AS "N_COMMENT"@description("Comment-changed"),
+     1000 AS "N_AREA"
 FROM {{ ref('SOURCE_DATA', 'NATION_TEST') }} "NATION_TEST"

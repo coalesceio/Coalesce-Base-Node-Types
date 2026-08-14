49c5b0cedeef66f6b7a831989a43a309d6446f1e
Anandhi <anandhi.sekaran@coalesce.io>
2026-08-14 17:14:07 +0530

    SQLWORK_REDEPLOY_20260814_1714

--- nodes/TARGET_DB-WRK_ORDERS314.sql
+++ nodes/TARGET_DB-WRK_ORDERS314.sql
@@ -2,8 +2,9 @@
 @nodeType("62f0cbc1-957e-4ad5-8ca2-5a2d8d6eef62")
 @truncateBefore
 SELECT
-     "ORDER_ID" AS "ORDER_ID",
-     "CUSTOMER_NAME" AS "CUSTOMER_NAME",
+     "ORDER_ID" AS "ORDER_IDS",
+     "CUSTOMER_NAME" AS "CUSTOMER_NAME" @defaultValue("hap"),
      "ORDER_DATA" AS "ORDER_DATA",
-     CURRENT_DATE() AS cur_date
+     current_timestamp() AS load_timestamp
 FROM {{ ref('SOURCE_DATA', 'ORDERS') }} "ORDERS"
+where order_ids > 0
